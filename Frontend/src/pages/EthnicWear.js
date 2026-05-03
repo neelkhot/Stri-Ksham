7096f30c5347d6acdb13a9d6f3c2f69f67005b63
Neel khot <neelkhot1008@gmail.com>
2026-05-03 23:30:22 +0530

    sari added in ethnic wear

--- Frontend/src/pages/EthnicWear.js
+++ Frontend/src/pages/EthnicWear.js
@@ -2,11 +2,11 @@ import React from "react";
 import CategoryCollectionPage from "./CategoryCollectionPage";
 
 const EthnicWear = () => (
-  <CategoryCollectionPage
-    title="Ethnic Wear"
-    category="Ethnic Wear"
-    emptyMessage="No ethnic wear products are available right now."
-  />
+ <CategoryCollectionPage
+  title="Ethnic Wear"
+  category={["Ethnic Wear", "Sari"]}
+  emptyMessage="No ethnic wear products are available right now."
+/>
 );
 
 export default EthnicWear;
